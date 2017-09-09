34aacbedc15694daf381c0b0cfae6c58184237f2
chhaymenghong <chhaymenghong@yahoo.com>
2017-09-09 00:09:14 -0700

    Add jquery to global namespace
    Bootstrap expects this to function properly

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 /** This is an entry file for browserify. Browserify will come here
  * and load all dependencies **/
+// defined without var so that it becomes global.Bootstrap expects jquery in the global namespace
+$ = jquery = require('jquery');
+
 var App = console.log('hi');
 module.exports = App;
